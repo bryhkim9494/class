eb35019daaea20d275c19ebeae3f67518d6d84e6
bryhkim9494 <bryhkim9494@gmail.com>
2023-04-04 17:24:50 +0900

    java day12

--- javaProject/FirstJava/src/quiz/chapter10/BadIdInputException.java
+++ javaProject/FirstJava/src/quiz/chapter10/BadIdInputException.java
@@ -3,7 +3,7 @@ package quiz.chapter10;
 public class BadIdInputException extends Exception {
 
 	public BadIdInputException(String message) {
-		super("사용자의 입력 값이 올바르지 않습니다.");
+		super();
 		
 	}
 
--- javaProject/FirstJava/src/quiz/chapter10/ExceptionTest.java
+++ javaProject/FirstJava/src/quiz/chapter10/ExceptionTest.java
@@ -7,25 +7,33 @@ public class ExceptionTest {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("아이디를 입력하세요");
 		String id = sc.nextLine();
-		
-		if(checkId(id)) {
-			System.out.println(id);
-		} else {
-			System.out.println("영문자와 숫자만 가능합니다.");
+
+		try {
+			for (int i = 0; i < id.length(); i++) {
+				char c = id.charAt(i);
+				if (!(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9')) {
+					BadIdInputException e = new BadIdInputException("사용자의 입력 값이 올바르지 않습니다.");
+					throw e;
+				}
+			}
 		}
-		
+
+		catch (BadIdInputException e) {
+			System.out.println("사용자의 입력 값이 올바르지 않습니다.");
+		}
 	}
+
 	static boolean checkId(String id) {
-		
+
 		boolean result = true;
-		for (int i= 0; i <id.length(); i++ ) {
+		for (int i = 0; i < id.length(); i++) {
 			char c = id.charAt(i);
-			if (!(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c>='0' && c <='9')) {
+			if (!(c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9')) {
 				result = false;
 				break;
 			}
 		}
-		
+
 		return result;
 	}
 }
