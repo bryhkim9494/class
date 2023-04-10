99978448145157411ee361c0b7fef71611f7c0ab
bryhkim9494 <bryhkim9494@gmail.com>
2023-04-10 18:20:03 +0900

    Create sys_create_user_20230410.sql

---
+++ sql/sys_create_user_20230410.sql
@@ -0,0 +1,14 @@
+-- 2023.04.10
+-- 주석
+
+
+-- 사용자 생성
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+-- 1. 다른사용자 -> 팝업메뉴 -> 사용자생성
+-- 2. 계정이름 c##himdeia -> c##생략하기위해서는 ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;을 실행시키면 생략하고 생성가능
+-- 3. 권한 부여 : connect,resource
+
+
+
+
