556560c2747b596da2d24efe343f68c85c307e53
bryhkim9494 <bryhkim9494@gmail.com>
2023-05-15 18:20:05 +0900

    spring day6

--- springProject/intellij/SpringMVC/src/main/java/com/hi/app/domain/UserRequest.java
+++ springProject/intellij/SpringMVC/src/main/java/com/hi/app/domain/UserRequest.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
+
 @NoArgsConstructor
 @Setter
 @Getter
@@ -17,5 +19,6 @@ public class UserRequest {
     private String keyword;
     private int tno;
     private int page;
+    private LocalDate date;
 
 }
