31cd2f3f9e424c6c1f490ef09b3fa237b05fa733
bryhkim9494 <bryhkim9494@gmail.com>
2023-05-16 18:17:19 +0900

    spring day7

--- springProject/intellij/DeptSpring/src/main/java/com/hi/deptspring/deptspring/mapper/DeptMapper.java
+++ springProject/intellij/DeptSpring/src/main/java/com/hi/deptspring/deptspring/mapper/DeptMapper.java
@@ -1,6 +1,7 @@
 package com.hi.deptspring.deptspring.mapper;
 
 import com.hi.deptspring.deptspring.domain.DeptDTO;
+import com.hi.deptspring.deptspring.domain.DeptRegistRequest;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,5 +13,12 @@ public interface DeptMapper {
 
     //deptno컬럼으로 검색  => DeptDTO
     DeptDTO selectByDeptno(int deptno);
+    // DeptRegistRequest를 받아서 insert 처리해주는 메소드가 필요함
+    int insertDept(DeptRegistRequest request);
+
+    // DeptDTO를 전달 받고 해당 데이터를 update 처리해주는 메소드
+    int updateDept(DeptDTO deptDTO);
+
+    int deleteDept(int deptno);
 
 }
